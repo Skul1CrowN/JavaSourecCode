c7b2769f22c1f9f87cc85c41da66668b038da172
Skul1CrowN <boat.72545@gmail.com>
2022-04-19 17:51:38 +0700

    remove commit

--- Lab11/Pro1_64010516/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.pro1_64010516 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.pro1_64010516 to javafx.fxml;
-    exports com.example.pro1_64010516;
-}
